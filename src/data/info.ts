66a352466792cb6d725d1e34fe2de22ba314fb1b
Amadou Boubacar Niang <71797475+niangamadou888@users.noreply.github.com>
2023-09-07 14:08:42 +0000

    Add files via upload

---
+++ src/data/info.ts
@@ -0,0 +1,173 @@
+export const info = {
+  baseUrl: "#",
+  name: "Amadou Boubacar Niang",
+  jobDescription: "Junior Software Developer",
+  about: `I am a highly motivated and driven 23-year-old student who is eager to take on new challenges that align with my career ambitions. 
+  With a strong work ethic and a passion for learning, 
+  I am committed to bringing my best to every project and working collaboratively within a team to achieve success. 
+  My portfolio showcases my ability to deliver quality work on time and with attention to detail. 
+  I am confident in my ability to adapt quickly to new situations and technologies. 
+  If you have any questions or would like to learn more about my qualifications, please don't hesitate to contact me. 
+  I look forward to the opportunity to contribute my skills and expertise to your team.`,
+
+  languages: [
+    {
+      name: "French",
+      level: "Native",
+    },
+    {
+      name: "English",
+      level: "B2",
+    },
+  ],
+
+  experience: [
+    {
+      name: "Internship at Hatice Technologie",
+      location: "Dakar, Sénégal",
+      startDate: "March 2022",
+      endDate: "May 2022",
+      description: [
+        "✓ Creation of a Tic Tac Toe game and a Sokoban game on Android mobile.",
+      ],
+    },
+
+    {
+      name: "Internship at SAED",
+      location: "Saint-Louis, Sénégal",
+      startDate: "March 2023",
+      endDate: "May 2023",
+      description: [
+        "✓ Network Administrator",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Cheikh Hamidou Kane Digital University(Ex UVS)",
+      location: "Saint-Louis, Sénégal",
+      startDate: "2019",
+      endDate: "Present",
+      description: [
+        "bachelor's degree in computer science and application development (web, mobile, gaming)",
+      ],
+    },
+    {
+      name: "André Peytavin Technical High School",
+      location: "Saint-Louis, Sénégal",
+      startDate: "2016",
+      endDate: "2019",
+      description: ["high school diploma in science and technology of economics and management"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "#",
+    twitter: "https://twitter.com/niangamadou888",
+    github: "https://github.com/niangamadou888",
+    email: "amadouniang2001@gmail.com",
+    linkedin: "https://www.linkedin.com/in/amadou-boubacar-niang-09b973160/",
+  },
+
+  certifications: [
+    {
+      name: "Network Administrator Certification",
+      issuedBy: "IMSAS",
+      issuedOn: "10 August 2023",
+      description: "Network Administration."
+    },
+    {
+      name: "Legacy Full Stack Developer",
+      issuedBy: "freeCodeCamp",
+      issuedOn: "29 May 2023",
+      verifyBadgeUrl: "https://www.credly.com/badges/d1f33225-699c-4453-aef4-e66d51283bca",
+      description: "Responsive Web Design, Front End Development Libraries, JavaScript Algorithms and Data Structures, Data Visualization, Back End Development and APIs, Scientific Computing with Python, Relational Database, Data Analysis with Python."
+    },  
+  ],
+
+  skills: [
+    {
+      name: "HTML"
+    },
+    {
+      name: "CSS"
+    },
+    {
+      name: "Bootstrap"
+    },
+    {
+      name: "JavaScript"
+    },
+    {
+      name: "React"
+    },
+    {
+      name: "Golang"
+    },
+    {
+      name: "C"
+    },
+    {
+      name: "C++"
+    },
+    {
+      name: "C#"
+    },
+    {
+      name: "MongoDB"
+    },
+    {
+      name: "Node.js"
+    },
+    {
+      name: "Python"
+    },
+    {
+      name: "SQL"
+    },
+    {
+      name: "Git"
+    },
+    {
+      name: "Linux"
+    },
+    {
+      name: "Visual Studio Code"
+    },
+    {
+      name: "Django"
+    },
+    {
+      name: "Unity3D"
+    },
+    {
+      name: "Unreal Engine"
+    },
+    {
+      name: "Android Studio"
+    },
+  ],
+
+  projects: [
+    {
+      title: "Sudoku Solver - Web Application",
+      isFeatured: true,
+      thumbnail: "/assets/images/sudoku-solver.png",
+      githubUrl: "https://github.com/niangamadou888/boilerplate-project-sudoku-solver",
+    },
+    {
+      title: "To Do List - Web Application",
+      isFeatured: true,
+      thumbnail: "/assets/images/todolist.png",
+      githubUrl: "https://github.com/niangamadou888/to-do-list-javascript",
+      liveUrl: "https://to-do-listabn.netlify.app/"
+    },
+    {
+      title: "American / British translator - Web Application",
+      isFeatured: true,
+      thumbnail: "/assets/images/translator.png",
+      githubUrl: "#",
+    },
+  ],
+};
